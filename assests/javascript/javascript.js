572f0b5c0e8f1394d0e1e76459b2aa3dbc07c39e
adventuroussheep <jacob_haas_7@hotmail.com>
2019-04-16 18:01:41 -0700

    reduced image sizes, added hamburger menu

--- assests/javascript/javascript.js
+++ assests/javascript/javascript.js
@@ -1,17 +1,47 @@
+$(document).ready(function() {
 
-$(document).ready(function(){    
-    
-window.onbeforeunload = function (){
-    window.scrollTo(0,0);
-}
+  window.onbeforeunload = function() {
+    window.scrollTo(0, 0);
+  };
 
-$("#linkedinUrl").on("click",function(){
+
+  $("#linkedinUrl").on("click", function() {
     window.open("https://www.linkedin.com/in/jacob-haas-909b05172/");
-})
+  });
 
-$("#githubUrl").on("click",function(){
+
+  $("#githubUrl").on("click", function() {
     window.open("https://github.com/adventuroussheep");
-})
+  });
 
 
+  $("#clickCopy").on("click", function() {
+    var copyText = document.querySelector("#copytext").innerText;
+    console.log(copyText);
+    // document.execCommand("copy");
+
+  });
+
+  
+  
+  
+  
+  // Scroll feature for anchors/dropdown menu
+  $(".anchor").click(function(e){
+      e.preventDefault();
+      var aid = $(this).attr("href");
+      $("html,body").animate({scrollTop: $(aid).offset().top},"slow");
+  });
+    
+    // email click
+    $("#emailImg").on("click",function(){
+        location.href = "mailto:jacob_haas@live.com";
+    })
 });
+
+  // Navbar toggle
+  function dropDownFunc(x) {
+    x.classList.toggle("change");
+    $("#dropDown").toggle(); 
+  };
+
